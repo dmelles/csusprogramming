b2980e213bb94db3bb46894c54cdab451aaebf11
Connor Soohoo <connorsoohoo@gmail.com>
2015-12-01 15:14:25 -0800

    Added Connor Soohoo to the file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld{
 		System.out.println("Hello World!");
 		System.out.println("ASDF");
 		System.out.println("justin-branch added");
+		System.out.println("soohoo-branch added");
 	}
 	
 }
